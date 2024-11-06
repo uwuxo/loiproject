aa82dcef83f480b5028c851023ff9810d2990d10
luchitam <giomoi77@gmail.com>
2024-11-06 07:58:00 +0700

    fix server 6

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,10 @@
 <?php
 ini_set('memory_limit', '512M');
+ini_set('max_execution_time', '3600');
+ini_set('max_input_time', '3600');
+ini_set('post_max_size', '64M');
+ini_set('upload_max_filesize', '64M');
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
